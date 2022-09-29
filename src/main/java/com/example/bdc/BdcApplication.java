7026162cf3c0587084e014418b56e2c9fef145df
professorik <36134599+professorik@users.noreply.github.com>
2022-09-29 16:31:29 +0300

    connected db, added service & controller

--- src/main/java/com/example/bdc/BdcApplication.java
+++ src/main/java/com/example/bdc/BdcApplication.java
@@ -2,12 +2,14 @@ package com.example.bdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties
 public class BdcApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BdcApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/example/bdc/domain/MyController.java
@@ -0,0 +1,16 @@
+package com.example.bdc.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+@Slf4j
+public class MyController {
+    @GetMapping("info")
+    public String getInfo() {
+        return "Hello, World!";
+    }
+}
