2e2c384e030fe75ef578e2dd3843ddd0b25d76ef
professorik <36134599+professorik@users.noreply.github.com>
2022-09-29 15:38:51 +0300

    Initial commit

---
+++ src/main/java/com/example/bdc/BdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bdc/BdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
